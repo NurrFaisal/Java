c72b82966a7fe89898606cca4bdd9448120e282e
Nur Mohammad Faisal <noormoy@gmail.com>
2022-05-09 12:24:08 +0600

    muliple exceptions catch 2

--- src/ExceptionsDemo/ExceptionDemo.java
+++ src/ExceptionsDemo/ExceptionDemo.java
@@ -1,19 +1,25 @@
 package ExceptionsDemo;
 
+import javax.script.SimpleBindings;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionDemo {
     public static void show(){
         try {
             var reader = new FileReader("file.txt");
             var value = reader.read();
+            new SimpleDateFormat().parse("");
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("File does not exit !!!");
         } catch (IOException e){
             System.out.println("Could not read data !!!");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
     }
 
