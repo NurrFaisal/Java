126559a7ade2a9602c4ce5dfb9946750a37dfc8d
Nur Mohammad Faisal <noormoy@gmail.com>
2022-05-09 12:18:28 +0600

    muliple exception catch

--- src/ExceptionsDemo/ExceptionDemo.java
+++ src/ExceptionsDemo/ExceptionDemo.java
@@ -2,17 +2,19 @@ package ExceptionsDemo;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.sql.SQLOutput;
 
 public class ExceptionDemo {
     public static void show(){
         try {
             var reader = new FileReader("file.txt");
+            var value = reader.read();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
+        } catch (IOException e){
+            System.out.println("Could not read data !!!");
         }
-        System.out.println("File Opened");
-
     }
 
 }
