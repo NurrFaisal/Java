1806cb75b241bf2853b0db644c3c59473feb2390
Nur Mohammad Faisal <noormoy@gmail.com>
2022-05-09 12:11:43 +0600

    FileNotFoundException

--- src/ExceptionsDemo/ExceptionDemo.java
+++ src/ExceptionsDemo/ExceptionDemo.java
@@ -1,12 +1,17 @@
 package ExceptionsDemo;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.SQLOutput;
 
 public class ExceptionDemo {
     public static void show(){
-        sayHello(null);
+        try {
+        var reader = new FileReader("file.txt");
+        System.out.println("File Opened");
+        } catch (FileNotFoundException ex){
+            System.out.println(ex.getMessage());
+        }
     }
-    public static void sayHello(String name){
-        System.out.println(name.toUpperCase());
-    }
+
 }
