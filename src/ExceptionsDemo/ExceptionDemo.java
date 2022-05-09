c2ae7cc6aa68a3af7ad52e91e24658bd14e2ac84
Nur Mohammad Faisal <noormoy@gmail.com>
2022-05-09 12:38:06 +0600

    The try with resouce statements

--- src/ExceptionsDemo/ExceptionDemo.java
+++ src/ExceptionsDemo/ExceptionDemo.java
@@ -3,6 +3,7 @@ package ExceptionsDemo;
 import javax.script.SimpleBindings;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLOutput;
 import java.text.ParseException;
@@ -10,20 +11,13 @@ import java.text.SimpleDateFormat;
 
 public class ExceptionDemo {
     public static void show(){
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("...");
+        ) {
             var value = reader.read();
         } catch (IOException e){
             System.out.println("Could not read data !!!");
-        } finally {
-            if (reader != null){
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
 }
