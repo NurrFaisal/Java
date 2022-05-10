ed94291a3f71f81cff203c6beeefe0b1bb6f7081
NurFaisal <nurfaisal@nexkraft.com>
2022-05-10 15:26:07 +0600

    constrains

--- src/codeWithMosh/Main.java
+++ src/codeWithMosh/Main.java
@@ -7,11 +7,6 @@ import generics.User;
 
 public class Main {
     public static void main(String[] args){
-        // int -> Integer
-        // float -> Float
-        // boolean -> Boolean
-        GenericList<Integer> numbers = new GenericList<>();
-        numbers.add(1); //Boxing
-        numbers.get(0); //Unboxing
+
     }
 }
