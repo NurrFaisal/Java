9b75462f87fdb3cb5f7437ef6e93d137e312f09a
NurFaisal <nurfaisal@nexkraft.com>
2022-05-10 12:15:34 +0600

    generic classes

--- src/codeWithMosh/Main.java
+++ src/codeWithMosh/Main.java
@@ -1,14 +1,17 @@
 package codeWithMosh;
 
 
+import generics.GenericList;
 import generics.List;
 import generics.User;
 
 public class Main {
     public static void main(String[] args){
-       var list = new List();
-       list.add(1);
-       list.add("1");
-       list.add(new User());
+//      var list = new GenericList<Integer>();
+      var list = new GenericList<User>();
+//      list.add(10);
+//      int number = list.get(0);
+      list.add(new User());
+      User user = list.get(0);
     }
 }
