6961420a4c326c6f6321351c0cd94df1c70d5d56
NurFaisal <nurfaisal@nexkraft.com>
2022-05-10 12:04:33 +0600

    ........

--- src/codeWithMosh/Main.java
+++ src/codeWithMosh/Main.java
@@ -2,10 +2,13 @@ package codeWithMosh;
 
 
 import generics.List;
+import generics.User;
 
 public class Main {
     public static void main(String[] args){
        var list = new List();
        list.add(1);
+       list.add("1");
+       list.add(new User());
     }
 }
